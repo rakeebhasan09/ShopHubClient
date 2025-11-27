1937396e24675aeab12c8a72fa0ad649045d4605
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-11-27 12:44:47 +0600

    server api updated.

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -3,8 +3,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	// baseURL: "https://first-next-jus-project-server.vercel.app",
-	baseURL: "http://localhost:5000",
+	baseURL: "https://first-next-jus-project-server.vercel.app",
 });
 
 const useAxios = () => {

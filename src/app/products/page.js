318c96bd0021cf672d244293fa6dbd651e37fdb1
rakeebhasan <mdrakeebhasan1@gmail.com>
2026-01-16 11:13:51 +0600

    Some Modification has been applied.

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -49,7 +49,7 @@ const Products = () => {
 					products={products}
 					setCategory={setCategory}
 				/>
-				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
 					{filterdProducts.map((product) => (
 						<ProductCard key={product._id} product={product} />
 					))}
